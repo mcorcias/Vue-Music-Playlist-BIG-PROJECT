b86bbe9214120b86985d6d121f71304e7d06bd79
mcorcias <mosescorcias@gmail.com>
2021-02-06 16:24:17 +0200

    getting playlist in real-time inside Home Component

--- music-project/src/views/Home.vue
+++ music-project/src/views/Home.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="home">
    <p>Homepage</p>
+   <div v-if="documents">
+     <h1 v-for="doc in documents" :key="doc.id">{{doc.title}}</h1>
+   </div>
+   <div v-if="error" class="error">{{error}}</div>
   </div>
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection'
 
 export default {
   name: 'Home',
-  components: {}
+  setup(){
+    const {error, documents} = getCollection('playLists')
+
+    return {error, documents}
+
+  }
 }
 </script>

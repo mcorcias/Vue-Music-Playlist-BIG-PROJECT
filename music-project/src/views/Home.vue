c3dc9422b651d8a1bab6c9ae4ccb783bec805583
mcorcias <mosescorcias@gmail.com>
2021-02-06 17:02:26 +0200

    add a new component - ListView - show all the play-list

--- music-project/src/views/Home.vue
+++ music-project/src/views/Home.vue
@@ -2,17 +2,19 @@
   <div class="home">
    <p>Homepage</p>
    <div v-if="documents">
-     <h1 v-for="doc in documents" :key="doc.id">{{doc.title}}</h1>
+      <ListView :playlists="documents" />
    </div>
    <div v-if="error" class="error">{{error}}</div>
   </div>
 </template>
 
 <script>
+import ListView from '../components/ListView.vue'
 import getCollection from '@/composables/getCollection'
 
 export default {
   name: 'Home',
+  components: { ListView },
   setup(){
     const {error, documents} = getCollection('playLists')
 

4a81d6938adb0ca820af2bb5e6d9fc1cfc62d5fc
mcorcias <mosescorcias@gmail.com>
2021-02-08 21:34:09 +0200

    add to useDocument composable a new function - updateDoc and use it in AddSong component to update new song

--- music-project/src/components/AddSong.vue
+++ music-project/src/components/AddSong.vue
@@ -12,11 +12,14 @@
 
 <script scoped>
 import { ref } from 'vue'
+import useDocument from '@/composables/useDocument'
 export default {
-    setup(){
+    props:['playlist'],
+    setup(props){
         const title = ref('')
         const artist = ref ('')
         const showForm = ref(false)
+        const {updateDoc} = useDocument('playLists',props.playlist.id)
         
         const handleSubmit = async()=>{
             const newSong = {
@@ -24,8 +27,12 @@ export default {
                 artist: artist.value,
                 id: Math.floor(Math.random() * 10000000)
             }
+            await updateDoc({
+                songs: [...props.playlist.songs,newSong]
+            })
 
-            console.log(newSong);
+            title.value=''
+            artist.value=''
         }
 
         return {title,artist,showForm,handleSubmit}
